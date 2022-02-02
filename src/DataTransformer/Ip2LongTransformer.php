5f335f78844483f3bdacc3d2f29c7f6e5de2cf85
Martin Kirilov <wucdbm@gmail.com>
2022-02-03 01:00:39 +0200

    fix ip2long form error path

--- src/DataTransformer/Ip2LongTransformer.php
+++ src/DataTransformer/Ip2LongTransformer.php
@@ -3,26 +3,33 @@
 namespace Wucdbm\SymfonyFormTypes\DataTransformer;
 
 use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
 
 class Ip2LongTransformer implements DataTransformerInterface {
 
-    public function transform($long) {
-        if (null === $long) {
+    public function transform($value) {
+        if (null === $value) {
             return null;
         }
 
-        $ip = long2ip($long);
+        $ip = long2ip($value);
 
-        return $ip ? $ip : null;
+        return $ip ?: null;
     }
 
-    public function reverseTransform($ip) {
-        if (!$ip) {
+    public function reverseTransform($value) {
+        if (!$value) {
             return null;
         }
 
-        $long = ip2long($ip);
+        if (false === filter_var($value, FILTER_VALIDATE_IP)) {
+            throw new TransformationFailedException(
+                sprintf('Provided IP "%s" is not a valid IP Address', $value)
+            );
+        }
 
-        return $long ? $long : null;
+        $long = ip2long($value);
+
+        return $long ?: null;
     }
 }
--- src/Form/Ip2LongType.php
+++ src/Form/Ip2LongType.php
@@ -5,24 +5,12 @@ namespace Wucdbm\SymfonyFormTypes\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormError;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Wucdbm\SymfonyFormTypes\DataTransformer\Ip2LongTransformer;
 
 class Ip2LongType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
-        $transformer = new Ip2LongTransformer();
-        $builder->addModelTransformer($transformer);
-
-        $builder->addEventListener(FormEvents::SUBMIT, static function (FormEvent $event) {
-            $form = $event->getForm();
-            $ip = $event->getData();
-            if (false === filter_var($ip, FILTER_VALIDATE_IP)) {
-                $form->addError(new FormError(sprintf('Provided IP "%s" is not a valid IP Address', $ip)));
-            }
-        });
+        $builder->addModelTransformer(new Ip2LongTransformer());
     }
 
     public function getParent() {
